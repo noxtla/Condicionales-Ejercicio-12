faeb09fb37aa58e72dc2ea9f5838deff78b90554
Jesus Salvador Cortes Gutierrez <31747651+noxtla@users.noreply.github.com>
2019-02-11 11:10:01 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,48 @@
+/*Determinar si un numero es multiplo de 10*/
 
+package Condicionales;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int nota;
+		
+		nota = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la nota"));
+		
+		/*if (nota > 7 && nota <10) {
+			JOptionPane.showMessageDialog(null, "Sobresaliente");
+		}
+		
+		if (nota > 5 && nota < 7) {
+			JOptionPane.showMessageDialog(null, "Notable");
+		}
+		
+		if (nota > 3 && nota < 5) {
+			JOptionPane.showMessageDialog(null, "Suficiente");
+		}*/
+		
+		switch (nota) {
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4: JOptionPane.showMessageDialog(null, "Insuficiente");
+		 	break;
+		case 5:
+		case 6:
+		case 7:JOptionPane.showMessageDialog(null, "Suficiente");
+			break;
+		case 8:
+		case 9:JOptionPane.showMessageDialog(null, "Notable");
+		case 10: JOptionPane.showMessageDialog(null, "Sobresaliente");
+
+
+		default:
+			break;
+		}
+		
+
+	}
+}
